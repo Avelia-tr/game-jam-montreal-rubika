5d3f50c09911aed64ba1c4530b648e73984e0d7f
Bard Avelia <a.bard@rubika-edu.com>
2026-02-05 12:13:27 -0500

    add basic logic and input handling

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,16 +1,104 @@
 using UnityEngine;
+using System;
+using System.Linq;
 
 public class Board : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // get input and do logic
+
+    Vector2Int PlayerPosition;
+
+    bool PlayerDead = false;
+
+    Vector2Int direction;
+    Vector2Int[] Boxes;
+
+    Tiles[][] tiles;
+
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(Vector2Int move)
     {
-        
+        if (PlayerDead) return;
+        MoveLogic(move);
+        // ticks ?
+        direction = move;
     }
+
+    public void MoveLogic(Vector2Int move)
+    {
+        var newPosition = PlayerPosition + move;
+
+        var boxCheck = IsBoxIn(newPosition);
+
+        if (boxCheck is null)
+        {
+            PlayerPosition = newPosition;
+            // Move Anim event ?
+            return;
+        }
+
+        var newBoxPosition = newPosition + move;
+
+        if (tiles[newBoxPosition.x][newBoxPosition.y] == Tiles.Wall)
+        {
+            // we cannot do the movement so everything stops
+            return;
+        }
+
+        // pushevent anim ?
+
+        Boxes[boxCheck.Value] = newBoxPosition;
+        PlayerPosition = newPosition;
+    }
+
+    public void Pull()
+    {
+        if (PlayerDead) return;
+        PullLogic();
+
+        PlayerDead = DeathCheck();
+        // raise tick event ?
+
+        // winCheck
+    }
+    public void PullLogic()
+    {
+        var boxToPull = PlayerPosition + direction;
+
+        var boxCheck = IsBoxIn(boxToPull);
+
+        if (boxCheck is null)
+        {
+            //nothing happens 
+            //empty pull event ?
+            return;
+        }
+
+        PlayerPosition -= direction;
+        Boxes[boxCheck.Value] = PlayerPosition;
+    }
+
+    bool DeathCheck() { throw new NotImplementedException(); }
+
+    bool WinCheck() { throw new NotImplementedException(); }
+
+    int? IsBoxIn(Vector2Int position)
+    {
+        for (int i = 0; i < Boxes.Length; i++)
+        {
+            if (Boxes[i] == position) return i;
+        }
+
+        return null;
+    }
+}
+
+enum Tiles
+{
+    None,
+    Wall,
+    Targets,
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeReference]
+    Board board;
+
+    void Start() { }
+
+    private void OnMove(InputValue _input)
     {
-        
+        var input = _input.Get<Vector2>();
+        board.Move(new(Mathf.RoundToInt(input.x), Mathf.RoundToInt(input.y)));
+
+        //automatize input on hold
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnPull(InputValue _input)
     {
-        
+        if (!_input.isPressed) return;
+        board.Pull();
     }
 }
