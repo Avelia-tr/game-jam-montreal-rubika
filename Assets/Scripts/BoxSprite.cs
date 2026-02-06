f16c9a7b9b9f3be97d8cefc045be47617b1024a0
Bard Avelia <a.bard@rubika-edu.com>
2026-02-05 20:59:58 -0500

    random reformat

--- Assets/Scripts/BoxSprite.cs
+++ Assets/Scripts/BoxSprite.cs
@@ -5,23 +5,15 @@ public class BoxSprite : MonoBehaviour, IBox
     public Board board { get; set; }
     public Vector2Int Position { get; set; }
 
-    void Awake()
-    {
-        Position = new(((int)transform.position.x), ((int)transform.position.z));
-    }
+    void Awake() { Position = new(((int)transform.position.x), ((int)transform.position.z)); }
 
-    void Start()
-    {
-        board.ticks += UpdatePosition;
-    }
+    void Start() { board.ticks += UpdatePosition; }
 
     void Update() { }
 
     void UpdatePosition()
     {
         //Do Animation/Tween ?
-        //
-
         var newPosition = new Vector3(Position.x, transform.position.y, Position.y);
 
         transform.position = newPosition;
